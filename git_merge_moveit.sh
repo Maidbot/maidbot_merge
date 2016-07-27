281935904ed810962ebc6288770d594b6cb57e94
Dave Coleman <davetcoleman@gmail.com>
2016-07-27 11:17:06 -0600

    Cleaned up script, added verification step

--- git_merge_moveit.sh
+++ git_merge_moveit.sh
@@ -5,6 +5,7 @@ echo "Automated MoveIt! Repo Merging Script"
 echo " by davetcoleman"
 echo " inspired by: https://saintgimp.org/2013/01/22/merging-two-git-repositories-into-one-repository-without-losing-file-history/"
 echo " Discussion: http://discourse.ros.org/t/migration-to-one-github-repo-for-moveit/266"
+echo " Requres all repos have kinetic/jade/indigo-devel, except *_resources and *_experiemental"
 echo "---------------------------------------"
 echo ""
 
@@ -30,41 +31,6 @@ export repo_ssh_to_merge=(
     http://github.com/davetcoleman/moveit_resources.git
     http://github.com/davetcoleman/moveit_experimental.git
 )
-export repo_branch_to_merge_kinetic=(
-    kinetic-devel #core
-    kinetic-devel #ros
-    kinetic-devel #planners
-    kinetic-devel #ikfast
-    kinetic-devel #plugins
-    kinetic-devel #setup assistant
-    kinetic-devel #commander
-    kinetic-devel # resources
-    kinetic-devel # experimental
-)
-
-export repo_branch_to_merge_jade=(
-    jade-devel #core
-    jade-devel #ros
-    jade-devel #planners
-    jade-devel #ikfast
-    jade-devel #plugins
-    jade-devel #setup assistant
-    jade-devel #commander
-    jade-devel # resources
-    jade-devel # experimental
-)
-
-export repo_branch_to_merge_indigo=(
-    indigo-devel #core
-    indigo-devel #ros
-    indigo-devel #planners
-    indigo-devel #ikfast
-    indigo-devel #plugins
-    indigo-devel #setup assistant
-    indigo-devel #commander
-    indigo-devel # resources
-    indigo-devel # experimental
-)
 
 NUM_REPOS=${#repo_names_to_merge[@]}
 NUM_BRANCHES=3
@@ -84,6 +50,7 @@ git checkout -b kinetic-devel-temporary-unique-name
 git branch -d master
 
 # All repos must have unique branches for I/J/K so we clone them and make unique branches
+# Currently *_resources and *_experiemental do not
 git clone http://github.com/ros-planning/moveit_resources.git
 cd moveit_resources
 hub remote add davetcoleman
@@ -92,6 +59,7 @@ git co -b jade-devel
 git co -b kinetic-devel
 git push davetcoleman --all -f
 cd ..
+
 git clone http://github.com/ros-planning/moveit_experimental.git
 cd moveit_experimental
 hub remote add davetcoleman
@@ -101,7 +69,8 @@ git co -b kinetic-devel
 git push davetcoleman --all -f
 cd ..
 rm -rf moveit_resources moveit_experimental
-read -p "wait" var1
+
+
 IGNORE_SUBFOLDERS="-I .git"
 
 for ((i=0;i<NUM_REPOS;i++)); do
@@ -115,21 +84,20 @@ for ((i=0;i<NUM_REPOS;i++)); do
 
     for ((j=0;j<NUM_BRANCHES;j++)); do
         if [ "$j" -eq "0" ]; then
-            git checkout kinetic-devel-temporary-unique-name
-            REPO_BRANCH=${repo_branch_to_merge_kinetic[$j]}
+            BRANCH_NAME=kinetic-devel
         elif [ "$j" -eq "1" ]; then
-            git checkout jade-devel-temporary-unique-name
-            REPO_BRANCH=${repo_branch_to_merge_jade[$j]}
+            BRANCH_NAME=jade-devel
         else
-            git checkout indigo-devel-temporary-unique-name
-            REPO_BRANCH=${repo_branch_to_merge_indigo[$j]}
+            BRANCH_NAME=indigo-devel
         fi
 
-        echo "Processing branch ${REPO_BRANCH}"
+        git checkout ${BRANCH_NAME}-temporary-unique-name
+
+        echo "Processing branch ${BRANCH_NAME}"
         #read -p "wait" var1
 
         # Merge the remote repo
-        git merge ${REPO_NAME}/${REPO_BRANCH} -m "Merging repo ${REPO_NAME} into main unified repo"
+        git merge ${REPO_NAME}/${BRANCH_NAME} -m "Merging repo ${REPO_NAME} into main unified repo"
 
         # Generate a list of subfolders to ignore when moving repos into their subfolder
         IGNORE_SUBFOLDERS="$IGNORE_SUBFOLDERS -I ${REPO_NAME}"
@@ -154,7 +122,6 @@ for ((i=0;i<NUM_REPOS;i++)); do
             echo "Renamed ${REPO_NAME} inside of ${REPO_NAME} back to original name"
             #read -p "Press any key to continue"
         fi
-
 
         # Commit the move
         git commit -m "Moved ${REPO_NAME} into subdirectory"
@@ -218,6 +185,35 @@ cp $SCRIPT_DIR/template/kinetic/moveit.rosinstall .
 cp $SCRIPT_DIR/template/.travis.run.sh .
 cp $SCRIPT_DIR/template/.gitignore
 git add -A && git commit -a -m "Added README, travis CI, and rosinstall file"
+
+# Verify repos are the same
+for ((i=0;i<NUM_REPOS;i++)); do
+    REPO_NAME=${repo_names_to_merge[$i]}
+    REPO_URL=${repo_ssh_to_merge[$i]}
+    echo "---------------------------------------"
+    echo "Verifying repo: ${REPO_NAME}"
+
+    for ((j=0;j<NUM_BRANCHES;j++)); do
+        if [ "$j" -eq "0" ]; then
+            BRANCH_NAME=kinetic-devel
+        elif [ "$j" -eq "1" ]; then
+            BRANCH_NAME=jade-devel
+        else
+            BRANCH_NAME=indigo-devel
+        fi
+
+        # Switch to the correct branch to test
+        git co $BRANCH_NAME
+
+        # Clone the original version
+        git clone ${REPO_URL} -b ${BRANCH_NAME}  ${REPO_NAME}_${BRANCH_NAME}
+        # Do not comapare git repos
+        rm -rf ${REPO_NAME}_${BRANCH_NAME}/.git
+
+        echo "Comparing $BRANCH_NAME:"
+        diff -r ${REPO_NAME} ${REPO_NAME}_${BRANCH_NAME}
+    done
+done
 
 # Push to Github
 #git push origin kinetic-devel -f
